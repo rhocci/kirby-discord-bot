cf56ea8313b28492a7ab8a3f1fc636c8e8cffb23
rhocci <4tres1@gmail.com>
2025-10-20 22:28:57 +0900

    chore: add token

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config';
+
+const token = process.env.DISCORD_TOKEN;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`봇 준비 완료! ${readyClient.user.tag}`);
+});
+
+client.login(token);
