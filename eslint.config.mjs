fe9f07905eeb325b193c664993139adee9c0bc3f
rhocci <4tres1@gmail.com>
2025-11-01 14:42:13 +0900

    chore: set formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,28 +3,20 @@ import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
 
 export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    ignores: ['**/exports-unused.ts'],
-    rules: {
-      'max-nested-callbacks': ['error', { max: 4 }],
-      'max-statements-per-line': ['error', { max: 2 }],
-      'no-console': 'off',
-      'no-empty-function': 'error',
-      'no-lonely-if': 'error',
-      'no-shadow': ['error', { allow: ['err', 'resolve', 'reject'] }],
-      'no-var': 'error',
-      'no-undef': 'off',
-      'no-unused-vars': 'warn',
-      'prefer-const': 'error',
-      yoda: 'error',
-    },
-    prettier,
-  }
+	js.configs.recommended,
+	{
+		files: ['**/*.{js,ts}'],
+		languageOptions: {
+			parser: tsParser,
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+		},
+		rules: {
+			'no-console': 'off',
+			'no-unused-vars': 'warn',
+			'no-undef': 'off',
+			'prefer-const': 'error',
+		},
+	},
+	prettier,
 ];
