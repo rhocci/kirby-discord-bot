7817d1af9a0047b9952417251da4795fd97604a2
rhocci <4tres1@gmail.com>
2025-11-14 02:37:03 +0900

    remove: test code

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,18 +1,11 @@
-import { Client, Events } from 'discord.js';
-import dayjs from 'dayjs';
 import { initSchedulers } from '@/scheduler/index.js';
+import { Client, Events } from 'discord.js';
 
 export default {
 	name: Events.ClientReady,
 	once: true,
 	execute(client: Client) {
 		console.log(`봇 준비 완료! ${client.user?.tag}`);
-		const today = dayjs().format('YYYY-MM-DD');
-
-		console.log(
-			`====================\n${today} 일간(00시)\n====================`,
-		);
-
 		initSchedulers(client);
 	},
 };
