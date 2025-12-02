7074b7fbb5e398503e710d73aa62602284a2fc0c
rhocci <4tres1@gmail.com>
2025-12-02 10:09:55 +0900

    fix: daily schedule error

--- src/scheduler/index.ts
+++ src/scheduler/index.ts
@@ -14,50 +14,50 @@ export function initSchedulers(client: Client) {
 	 * 2. 당일 공결신청 스레드 생성/예시 템플릿 고정
 	 * 3. 전날 스레드 권한 조정(가능하면)
 	 */
-	nodeCron.schedule('0 15 * * 0-4', () => {
+	nodeCron.schedule('0 15 * * 0-4', async () => {
 		const today = dayjs().tz('Asia/Seoul').format('YYYY-MM-DD');
 
 		console.log(
 			`====================\n${today} 일간(00시)\n====================`,
 		);
 
-		initDailyAttendance();
-		createDailyThread(client);
+		await initDailyAttendance();
+		await createDailyThread(client);
 	});
 
 	/** 평일 12시 태스크
 	 * 1. 점심식사 알림
 	 */
-	nodeCron.schedule('0 3 * * 1-5', () => {
+	nodeCron.schedule('0 3 * * 1-5', async () => {
 		const today = dayjs().tz('Asia/Seoul').format('YYYY-MM-DD');
 
 		console.log(
 			`====================\n${today} 일간(12시)\n====================`,
 		);
-		alertLunchTime(client, 'start');
+		await alertLunchTime(client, 'start');
 	});
 
 	/** 평일 13시 태스크
 	 * 1. 점심식사 종료 알림
 	 */
-	nodeCron.schedule('0 4 * * 1-5', () => {
+	nodeCron.schedule('0 4 * * 1-5', async () => {
 		const today = dayjs().tz('Asia/Seoul').format('YYYY-MM-DD');
 
 		console.log(
 			`====================\n${today} 일간(13시)\n====================`,
 		);
-		alertLunchTime(client, 'end');
+		await alertLunchTime(client, 'end');
 	});
 
 	/** 주간(토요일) 12시 태스크
 	 * 1. 금주 출석현황/지각비 정산 통계 업로드
 	 */
-	nodeCron.schedule('0 15 * * 5', () => {
+	nodeCron.schedule('0 15 * * 5', async () => {
 		const today = dayjs().tz('Asia/Seoul');
 
 		console.log(
 			`====================\n${today.format('YYYY-MM-DD')} 주간(00시)\n====================`,
 		);
-		createWeeklyStats(client);
+		await createWeeklyStats(client);
 	});
 }
